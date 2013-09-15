9e7a099939c35028ac2f4968854bb18da84627f8
David Speck <david@saduras.de>
2013-09-15 13:30:09 +0200

    Create visual protal effect

---
+++ Assets/ThinkWithPortals/Scripts/PortalEffect.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PortalEffect : MonoBehaviour {
+	
+	public GameObject destination;
+	
+	void OnTriggerStay(Collider other) {
+		if( (other.transform.position - transform.position).magnitude < 0.1f ) {
+			other.transform.position = destination.transform.position;	
+		}
+	}
+	
+}
