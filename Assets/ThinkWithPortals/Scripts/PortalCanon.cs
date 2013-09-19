e4d4dc1cd96d6ec3bfd80731e2786caa6f77a72f
David Speck <david@saduras.de>
2013-09-19 13:18:56 +0200

    Prototyp Portal Canon

---
+++ Assets/ThinkWithPortals/Scripts/PortalCanon.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PortalCanon : MonoBehaviour {
+	
+	public GameObject bluePortalProjectil;
+	public GameObject orangePortalProjectil;
+	public float projectilImpuls;
+	
+	private GameObject barrel;
+	
+	// Use this for initialization
+	void Start () {
+		barrel = transform.Find("Main Camera/Barrel").gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Shoot blue portal on left click
+		if(Input.GetButtonDown("Fire1")) {
+			GameObject handle = Instantiate(bluePortalProjectil, barrel.transform.position, transform.localRotation) as GameObject;
+			handle.rigidbody.AddForce( barrel.transform.forward * projectilImpuls, ForceMode.Impulse);
+		}
+		
+		// Shoot orange portal on right click
+		if(Input.GetButtonDown("Fire2")) {
+			GameObject handle = Instantiate(orangePortalProjectil, barrel.transform.position, transform.localRotation) as GameObject;
+			handle.rigidbody.AddForce( barrel.transform.forward * projectilImpuls, ForceMode.Impulse);
+		}
+	}
+}
---
+++ Assets/ThinkWithPortals/Scripts/PortalCreator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PortalCreator : MonoBehaviour {
+	
+	public GameObject portalPrefab;
+	
+	void OnCollisionEnter(Collision collision) {
+		// Check if we hit a valid ground for an portal
+		// We tagged all valid areas with "PortalGround"
+		if( collision.gameObject.CompareTag("PortalGround") ) {
+			// search for an existing portal
+			GameObject portal = GameObject.Find(portalPrefab.name);
+			
+			if( portal == null ) {
+				// create new portal if we didn't find one
+				GameObject handle = Instantiate(portalPrefab, transform.position, collision.transform.rotation * Quaternion.AngleAxis(90f, new Vector3(1,0,0)) ) as GameObject;
+				// get rid of the "(clone)" name extension
+				handle.name = portalPrefab.name;
+			} else {
+				// move exiting portal to new position
+				portal.transform.position = transform.position;
+				portal.transform.rotation = collision.transform.rotation * Quaternion.AngleAxis(90f, new Vector3(1,0,0));
+			}
+		}
+		
+		// Destroy this projectil
+		Destroy( gameObject );
+	}
+}
