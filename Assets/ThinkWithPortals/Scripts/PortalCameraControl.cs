35b32610368c4e5c65eef09a2d1941daa10001d9
David Speck <david@saduras.de>
2013-09-20 19:50:51 +0200

    proper portal camera movement
    - blue portal got a rendered texture
    - orange portal got a camera
    - orange camera moves properly relative with main camera

---
+++ Assets/ThinkWithPortals/Scripts/PortalCameraControl.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PortalCameraControl : MonoBehaviour {
+	
+	public GameObject otherPortal;
+	
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// use offset of main camera to other portal to place this portal view camera
+		Vector3 posOffset = otherPortal.transform.InverseTransformPoint( Camera.main.transform.position );
+		
+		transform.localPosition =  - posOffset;
+		Debug.DrawLine( transform.parent.position, transform.position );
+		Debug.DrawLine( otherPortal.transform.position, otherPortal.transform.TransformPoint( posOffset )  );
+		
+		camera.nearClipPlane = posOffset.magnitude;
+		
+		Quaternion rotOffset = otherPortal.transform.rotation;
+		Quaternion rotCorrection = Quaternion.AngleAxis(180f, new Vector3(0,1,0));
+		transform.localRotation = rotCorrection * Quaternion.Inverse( rotOffset ) * Camera.main.transform.rotation;
+	}
+}
